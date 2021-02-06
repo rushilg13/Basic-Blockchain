5945aa20adb55b4254fe670092303239288cca3f
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-02-06 14:04:37 +0530

    Added Encapsulation

--- blockchain.py
+++ blockchain.py
@@ -11,10 +11,26 @@ genesis_block = Block("Chancellor on Brink of Second Bailout for Banks", ["Austi
 first_block = Block(genesis_block.block_hash, ["Tairoon sent 1 BTC to Kiara", "Nancy sent 11 BTC to Meyer"])
 second_block = Block(first_block.block_hash, ["Brian sent 8 BTC to Poppy"])
 
-print("hash of genesis_block:")
-print(genesis_block.block_hash)
-print("hash of first_block:")
-print(first_block.block_hash)
-print("hash of second_block:")
-print(second_block.block_hash)
+# print("hash of genesis_block:")
+# print(genesis_block.block_hash)
+# print("hash of first_block:")
+# print(first_block.block_hash)
+# print("hash of second_block:")
+# print(second_block.block_hash)
 
+print("\nThis program is only for adding blocks to the blockchain.")
+while(1):
+    transaction_list = []
+    latest_hash = genesis_block.block_hash
+    sender = input("Enter Sender's name: ")
+    receiver = input("Enter Receiver's name: ") 
+    amount = input("Enter amount of BTC to transfer: ")
+    transaction = sender + " sent " + amount + " BTC to " + receiver
+    transaction_list.append(transaction)
+    add_more = input("Do you want to add more transactions? Enter Yes/No.: ")
+    if (add_more == "Yes"):
+        continue
+    elif (add_more == "No"):
+        new_block = Block(latest_hash, transaction_list)
+        print("New block was created and added to the chain!")
+        print("Press ctrl+c to terminate program!")
