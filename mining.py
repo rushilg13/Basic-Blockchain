e449484c639489853b814eee18f2f62738243ca4
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-02-09 01:52:14 +0530

    Added mining.py

--- mining.py
+++ mining.py
@@ -12,6 +12,7 @@ def mining(Block_Number, transactions, previous_hash, nonce):
         hash_str = str(Block_Number) + transactions + previous_hash + str(nonce)
         new_hash = hash_gen(hash_str)
         if new_hash.startswith(prefix_str):
+                print("Miner is rewarded with 6.25 BTC!")
                 return new_hash
 
     print ('Increase Range!')
@@ -20,6 +21,7 @@ def mining(Block_Number, transactions, previous_hash, nonce):
 
 prefix_zero = 6
 start = time.time()
-print("Mining Started")
+print("Time Counter started!")
+print("Mining Started!")
 print(mining(9,"Austin sends 10 BTC to Zara, Medusa sends 20 BTC to Zeus", ((prefix_zero*'0')+'2a90a5765df425a4898b542d249852a7a5d599cd6dce4dfdbe0f195b10c65a65'), prefix_zero))
-print("Mining took", time.time()-start, "seconds")
+print("Mining took", time.time()-start, "Seconds")
